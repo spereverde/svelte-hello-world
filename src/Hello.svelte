8029b1ac71654d79204a5ebb6fb2a4ef5b985450
Kim Paulissen <kim.paulissen@kuleuven.be>
2021-06-28 22:09:09 +0200

    use props and add styling

--- src/Hello.svelte
+++ src/Hello.svelte
@@ -1,11 +1,21 @@
 <script>
-  let name = "kim";
+  export let name = "kim";
 </script>
 
-<p>Hello {name}!</p>
+<div class="card">
+  <p>This a test component from {name}!</p>
+</div>
 
 <style>
+  .card {
+    background: #333;
+    margin: 1em;
+    padding: 1em;
+    font-family: "Dancing Script", cursive;
+    font-size: xx-large;
+    box-shadow: grey 10px 10px 10px;
+  }
   p {
-    color: darkgoldenrod;
+    color: white;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Hello from "./Hello.svelte";
 
 var hello = new Hello({
   target: document.getElementById("here"),
+  props: {
+    name: "Konijn",
+  },
 });
 
 export default hello;
