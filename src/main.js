cedbe349a1894d29764a808845686d7eaa7fc874
Kim Paulissen <kim.paulissen@kuleuven.be>
2021-06-29 09:29:47 +0200

    add nested component with custom html tag

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+// import App from "./App.svelte";
 import Hello from "./Hello.svelte";
+import Nested from "./Nested.svelte";
 
 var hello = new Hello({
   target: document.getElementById("here"),
@@ -11,4 +13,27 @@ document.querySelector("#kim").addEventListener("click", function () {
   hello.$set({ name: "Kim" });
 });
 
-export default hello;
+var all_nested_elements = document.getElementsByTagName("nested");
+for (let item of all_nested_elements) {
+  //   console.log(item);
+  var nested = new Nested({
+    target: item,
+  });
+  if (item.hasAttribute("answer")) {
+    var answer = item.getAttribute("answer");
+    nested.$set({ answer: answer });
+  }
+}
+
+// if there is only 1 <nested> element
+// var answer = document.querySelector("nested").getAttribute("answer");
+// nested.$set({ answer: answer });
+
+// export default hello;
+
+// var app = new App({
+//   target: document.getElementById("appke"),
+// });
+// export default app;
+
+export default nested;
